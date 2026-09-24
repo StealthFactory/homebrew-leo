211889d648aef550eee20c2bf135593669b81669
Mohnish Thallavajhula <hi@iam.mt>
2026-09-24 14:04:54 -0700

    Release v0.3.3

--- Formula/leo.rb
+++ Formula/leo.rb
@@ -1,8 +1,8 @@
 class Leo < Formula
   desc "Personal, extensible task-runner CLI with a JSON object store"
   homepage "https://github.com/StealthFactory/leo"
-  url "https://github.com/StealthFactory/leo/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "0a68f7c3b1f06d19e3090a9e5e741c1b15a456a9b9fe38011114c28520e7fa5a"
+  url "https://github.com/StealthFactory/leo/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "8c3f1ba74536439e6dbcec3ee5a3465b23950c4bb9cf2f7d67e654a09b35cd90"
   license "MIT"
   head "https://github.com/StealthFactory/leo.git", branch: "main"
 
